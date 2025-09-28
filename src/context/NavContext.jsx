1e0cedbea22b505404d99dc9a2bfe6d6e377d1ff
Aryan Mangrule <mangrulearyan402@gmail.com>
2025-09-28 17:45:42 +0530

    Fixed import path issues and updated pages for Vercel

--- src/context/NavContext.jsx
+++ src/context/NavContext.jsx
@@ -1,8 +1,8 @@
 import React, {  useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
-// export const NavbarContext = createContext()
-// export const NavbarColorContext = createContext()
+export const NavbarContext = createContext()
+export const NavbarColorContext = createContext()
 
 const NavContext = ({ children }) => {
 
